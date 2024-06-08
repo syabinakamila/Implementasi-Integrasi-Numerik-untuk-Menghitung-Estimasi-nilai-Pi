6c98cfc7962938a14ab22392552d84dfb996eded
syabinakamila <mrszuraa@gmail.com>
2024-06-08 19:28:41 +0700

    Update kodesumber.py

--- kodesumber.py
+++ kodesumber.py
@@ -14,3 +14,10 @@ def simpson_integral(f, a, b, N):
 
 def f(x):
     return 4 / (1 + x**2)
+    
+#Dataset yang digunakan adalah nilai-nilai N yang berbeda yaitu [10, 100, 1000, 10000]. Hasil dari pengujian ini menghasilkan sebuah DataFrame
+ #      N  Approximation       RMS Error  Execution Time
+#0     10       3.142425  8.325377e-04        0.000098
+#1    100       3.141593  3.430682e-07        0.000199
+#2   1000       3.141593  3.424119e-11        0.002071
+#3  10000       3.141593  2.691930e-15        0.019881
